956316ec1ab70110541e7eedb2513e19600f782e
JannerChang <jannerchang@me.com>
2015-11-25 17:00:58 +0800

    后台运行
    改成后台运行

--- watchOmnifocus.sh
+++ watchOmnifocus.sh
@@ -1,1 +1,2 @@
-fswatch -o /Users/janner/Library/Containers/com.omnigroup.OmniFocus2/Data/Library/Caches/com.omnigroup.OmniFocus2 | xargs -n1 -I{} ~/AppleScript/of.sh
+#!/bin/sh
+nohup fswatch -o ~/Library/Containers/com.omnigroup.OmniFocus2/Data/Library/Caches/com.omnigroup.OmniFocus2 | xargs -n1 -I{} ~/AppleScript/OmnifocusTaskDoneSetFolderTags.sh 2 > /dev/null &
